37070c273f221e324d1df79d95a09410f31336cb
DonateAngel <705025@student.dusd.net>
2022-11-16 09:57:06 -0800

    Added collectable strawberrys
    Strawberry added cannot collect

--- RubysAdventureDonate/Assets/Scripts/RubyController.cs
+++ RubysAdventureDonate/Assets/Scripts/RubyController.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class RubyController : MonoBehaviour
 {
     public float speed = 3.0f;
+
     public int maxHealth = 5;
+
+    public int health { get { return currentHealth;}}
     int currentHealth;
 
     Rigidbody2D rigidbody2d;
@@ -15,7 +18,6 @@ public class RubyController : MonoBehaviour
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
-
         currentHealth = maxHealth;
     }
     // Update is called once per frame
@@ -27,15 +29,15 @@ public class RubyController : MonoBehaviour
     void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
     }
-    void ChangeHealth(int amount)
+    public void ChangeHealth(int amount)
     {
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-        Debug.Log(currentHealth + "/" + maxHealth); 
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 
 }
