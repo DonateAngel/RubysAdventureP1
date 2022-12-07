8843942e21e4048cfb41dd3b4aa8515ec4d297f5
DonateAngel <705025@student.dusd.net>
2022-12-07 09:56:53 -0800

    Fixed errors
    Finished projetile and fixed the errors. Stilll have the issue with the editors, also everything goes to the right, need to fix camera

--- RubysAdventureDonate/Assets/Scripts/RubyController.cs
+++ RubysAdventureDonate/Assets/Scripts/RubyController.cs
@@ -15,6 +15,7 @@ public class RubyController : MonoBehaviour
     bool isInvincible;
     float invincibleTimer;
 
+    public GameObject projectilePrefab;
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
@@ -26,16 +27,18 @@ public class RubyController : MonoBehaviour
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         currentHealth = maxHealth;
     }
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+      
         Vector2 move = new Vector2(horizontal, vertical);
 
-        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
         {
             lookDirection.Set(move.x, move.y);
             lookDirection.Normalize();
@@ -43,6 +46,7 @@ public class RubyController : MonoBehaviour
         animator.SetFloat("Look X", lookDirection.x);
         animator.SetFloat("Look Y", lookDirection.y);
         animator.SetFloat("Speed", move.magnitude);
+
         if (isInvincible)
         {
             invincibleTimer -= Time.deltaTime;
